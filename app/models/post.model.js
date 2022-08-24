8d50c04a3291a75235646d4e7bff6a98890e8110
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2022-08-24 14:05:54 +0700

    manbah post.model.js

---
+++ app/models/post.model.js
@@ -0,0 +1,20 @@
+module.exports =(mongoose) => {
+    const schema = mongoose.schema(
+        {
+            title:String,
+            body: String,
+            published: Boolean ,
+        }, 
+        {
+            timestamps: true
+        }
+    )
+    schema.method("toJSON", function(){
+        const {_v, _id, ...object} = this.toObject()
+        object.id = _id 
+        return object
+    })
+
+    const Post = mongoose.model("posts", schema)
+    return Post
+}
