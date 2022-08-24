552f10a32cdf7bb15f515606a2aa15c304970a09
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2022-08-24 11:48:51 +0700

    add get /

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.get('/', (req, res) => {
+    res.json({
+        message:"Welcome to tengku express js"
+    })
+})
+
 const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}` )
